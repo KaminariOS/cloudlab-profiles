878822c72b18d8bdd1818b97c0c26ab1c0b1b9fe
kosumi <chankocyo@gmail.com>
2024-09-16 11:37:54 -0400

    Update settings

--- profile.py
+++ profile.py
@@ -12,7 +12,7 @@ pc = portal.Context()
 request = pc.makeRequestRSpec()
 
 node_0 = request.RawPC('node-0')
-node_0.hardware_type = 'c4130'
+node_0.hardware_type = 'c240g5'
 node_0.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
 # bs = node_0.Blockstore("bs", "/mydata")
 # bs.size = "512GB"
