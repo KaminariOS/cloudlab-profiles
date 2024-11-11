923581683fffcf2cf453543fcc1e39a33a0dfb81
kosumi <chankocyo@gmail.com>
2024-11-11 10:58:47 -0500

    Use Ubuntu 18

--- profile.py
+++ profile.py
@@ -13,7 +13,7 @@ request = pc.makeRequestRSpec()
 
 node_0 = request.RawPC('node-0')
 node_0.hardware_type = 'c240g5'
-node_0.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
+node_0.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
 bs = node_0.Blockstore("bs", "/mydata")
 bs.size = "512GB"
 
