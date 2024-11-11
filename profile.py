6b8409c54739e58a95184f3b0b71302c4021be2e
kosumi <chankocyo@gmail.com>
2024-11-11 10:48:08 -0500

    Use Ubuntu 18

--- profile.py
+++ profile.py
@@ -14,8 +14,8 @@ request = pc.makeRequestRSpec()
 node_0 = request.RawPC('node-0')
 node_0.hardware_type = 'c240g5'
 node_0.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
-# bs = node_0.Blockstore("bs", "/mydata")
-# bs.size = "512GB"
+bs = node_0.Blockstore("bs", "/mydata")
+bs.size = "512GB"
 
 # Install and execute a script that is contained in the repository.
 node_0.addService(pg.Execute(shell="sh", command="/local/repository/dramhit-top.sh"))
